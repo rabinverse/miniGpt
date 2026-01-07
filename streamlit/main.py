50816d78e4d16524fdbfdd042561b3cfc22eb8ea
rabinverse <poudelrabin2004@gmail.com>
2026-01-07 12:57:31 +0545

    error handling script

--- streamlit/main.py
+++ streamlit/main.py
@@ -14,52 +14,53 @@ if "model" not in st.session_state:
     model.eval()
     st.session_state.model = model
 
+st.set_page_config(
+    page_title="MiniGPT Text Generator",
+    layout="centered"
+)
+
 st.title(
     "Text Generator (MiniGpt)",
     help="Imitates the text paragraph (The input paragraph provided)",
 )
 
 # prompt
-if "prompt" not in st.session_state:
-    st.session_state.prompt = ""
-st.session_state.prompt = st.text_area(
+prompt = st.text_area(
     "Enter starting text:",
-    value=st.session_state.prompt,
     placeholder="Sunday",
     help="Enter any letter or words or paragraph",
 )
-
 #  tokens slider
-if "max_tokens" not in st.session_state:
-    st.session_state.max_tokens = 250
-st.session_state.max_tokens = st.slider(
-    "Max new tokens to generate:", 50, 950, st.session_state.max_tokens,help="This project uses character level tokenization"
+
+max_tokens = st.slider(
+    "Max new tokens to generate:",
+    min_value=50,
+    max_value=950,
+    value=250,
+    help="This project uses character level tokenization",
 )
 
-# Generated output
-if "generated_text" not in st.session_state:
-    st.session_state.generated_text = ""
 
 # Generate button
 if st.button("Generate"):
-    if st.session_state.prompt.strip():
+    if prompt.strip():
         with st.spinner("Generating text..."):
-            idx = torch.tensor(
-                [encode_txt(st.session_state.prompt)], dtype=torch.long, device=device
-            )
+            try:
+                idx = torch.tensor(
+                    [encode_txt(prompt)], dtype=torch.long, device=device
+                )
+            except KeyError as e:
+                st.error("Please enter English text only. Other scripts are not supported.")
+                st.divider()
+                st.stop()
             with torch.no_grad():
                 generated_idx = st.session_state.model.generate(
-                    idx, max_new_tokens=st.session_state.max_tokens
+                    idx, max_new_tokens=max_tokens
                 )
-            st.session_state.generated_text = decode_txt(generated_idx[0].tolist())
+            generated_text = decode_txt(generated_idx[0].tolist())
+            st.divider()
+            st.header("Model Response: ")
+            st.code(generated_text, language="text")
+
     else:
         st.warning("Please enter any English text.")
-
-# Display generated text
-if st.session_state.generated_text:
-    st.text_area(
-        "Generated Text:",
-        value=st.session_state.generated_text,
-        height=400,
-        key="output_text_area",
-    )
